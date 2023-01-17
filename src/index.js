5925e8cd25c9885504063f79b0d5c4684b5c1256
Andrei Krautsou <109202914+Krautsou-Andrei@users.noreply.github.com>
2023-01-18 00:17:19 +0300

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const bracketArray = bracketsConfig.map((cur) => cur.join(""));
+
+  while (true) {
+    let newString = str;
+    for (let i = 0; i < bracketArray.length; i++) {
+      str = str.replace(bracketArray[i], "");
+    }
+    if (newString === str) break;
+  }
+  return !Boolean(str);
+};
